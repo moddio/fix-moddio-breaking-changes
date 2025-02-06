18e5d37a65767b00123c8088943139d443851376
MoeThun <dhtmitmi@gmail.com>
2025-02-06 23:00:44 +0800

    feat: add fixScale

---
+++ src/scale.ts
@@ -0,0 +1,67 @@
+import jsonfile from 'jsonfile'
+import fs from 'fs'
+import rfdc from 'rfdc'
+import { modifyCertainKey } from './utils/object';
+
+fs.readdirSync('./input').map(fileName => {
+  jsonfile.readFile(`./input/${fileName}`, function (err, obj: Record<string, any>) {
+    if (err) {
+      console.error(fileName, err);
+    }
+    let output = rfdc()(obj);
+
+    ['x', 'y', 'z', 'width', 'height', 'depth'].forEach(key => {
+      output = modifyCertainKey(output, key,
+        {
+          parentKeys: [],
+          currentParentKey: '',
+          targetParentKey: ['position', 'size'],
+          insideParentKeys: ['bodies'],
+          excludeKeys: ['z-index', 'rotate', 'mountRotation'],
+          brotherEntries: [["dataType", "region"]],
+          parent: {}
+        }, (v) => {
+          if (typeof v[key] === 'number' || typeof v[key] === 'string') {
+            v[key] = Number(v[key]) / 64
+          }
+        });
+    });
+
+    ['default'].forEach(key => {
+      output = modifyCertainKey(output, key,
+        {
+          parentKeys: [],
+          currentParentKey: '',
+          targetParentKey: ['zoom'],
+          insideParentKeys: [],
+          excludeKeys: [],
+          brotherEntries: [],
+          parent: {}
+        }, (v) => {
+          if (typeof v[key] === 'number' || typeof v[key] === 'string') {
+            v[key] = Number(v[key]) / 64
+          }
+        });
+    });
+
+    ['min', 'max', 'value'].forEach(key => {
+      output = modifyCertainKey(output, key,
+        {
+          parentKeys: [],
+          currentParentKey: '',
+          targetParentKey: ['speed'],
+          insideParentKeys: [],
+          excludeKeys: [],
+          brotherEntries: [],
+          parent: {}
+        }, (v) => {
+          if (typeof v[key] === 'number' || typeof v[key] === 'string') {
+            v[key] = Number(v[key]) / 64
+          }
+        });
+    });
+
+    console.log(fileName, ' Done!');
+    jsonfile.writeFileSync(`./output/${fileName}`, output);
+  });
+})
--- src/utils/object.ts
+++ src/utils/object.ts
@@ -16,7 +16,7 @@ export const removeCertainKey = (obj: AnyObj, targetKey: string, parentKey?: { c
         if (nowParentKey) {
           nowParentKey.currentParentKey = k;
         }
-        newObj[k] = removeCertainKey(v, targetKey, nowParentKey)
+        newObj[k] = removeCertainKey(v, targetKey, nowParentKey, effect)
         break;
       }
 
@@ -26,6 +26,76 @@ export const removeCertainKey = (obj: AnyObj, targetKey: string, parentKey?: { c
             should_trigger_effect = true;
             break;
           }
+        }
+
+        newObj[k] = v;
+        break;
+      }
+    }
+  }
+  if (should_trigger_effect) {
+    effect?.(newObj);
+  }
+  return newObj;
+}
+
+export const modifyCertainKey = (obj: AnyObj,
+  targetKey: string,
+  parentKey?: {
+    parentKeys: string[],
+    currentParentKey: string,
+    targetParentKey: string[],
+    insideParentKeys: string[],
+    excludeKeys: string[],
+    brotherEntries: [string, any][],
+    parent: AnyObj,
+  },
+  effect?: (obj: AnyObj) => void,
+  force = false
+): AnyObj => {
+  const newObj: AnyObj = Array.isArray(obj) ? [] : {}
+  let should_trigger_effect = false;
+  if (obj === undefined || obj === null) {
+    return obj;
+  }
+
+  if (parentKey) {
+    parentKey.parent = obj;
+  }
+
+  for (let [k, v] of Object.entries(obj)) {
+    switch (typeof v) {
+      case 'object': {
+        const nowParentKey = rfdc()(parentKey);
+        if (nowParentKey) {
+          nowParentKey.parentKeys.push(k);
+          nowParentKey.currentParentKey = k;
+        }
+        let force_to_update = false;
+
+        if (parentKey === undefined || (!parentKey.excludeKeys.includes(parentKey.currentParentKey) && parentKey.brotherEntries.some(([k, v]) => parentKey.parent[k] === v))) {
+          force_to_update = true;
+        }
+
+        newObj[k] = modifyCertainKey(v, targetKey, nowParentKey, effect, force || force_to_update)
+        break;
+      }
+
+      default: {
+        if (parentKey === undefined || (!parentKey.excludeKeys.includes(parentKey.currentParentKey) && parentKey.targetParentKey.includes(parentKey.currentParentKey))) {
+          if (targetKey === k) {
+            should_trigger_effect = true;
+          }
+        }
+
+        if (parentKey === undefined || (!parentKey.excludeKeys.includes(parentKey.currentParentKey) && parentKey.insideParentKeys.some(key => !parentKey.excludeKeys.includes(key) && parentKey.parentKeys.includes(key)))) {
+          if (targetKey === k) {
+            should_trigger_effect = true;
+          }
+        }
+
+        if(force) {
+          should_trigger_effect = true;
         }
 
         newObj[k] = v;
